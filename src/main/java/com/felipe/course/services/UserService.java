41e6ebb73bc6136831e6d1c694ea2dfe610e6104
FelipePereiraBL <felipereirabl@gmail.com>
2021-11-23 17:16:59 -0300

    Atualização de usuário com PUT

--- src/main/java/com/felipe/course/services/UserService.java
+++ src/main/java/com/felipe/course/services/UserService.java
@@ -40,11 +40,24 @@ public class UserService
 		repository.deleteById(id);
 	}
 	
+	public User update(User obj)
+	{
+		User newObj=findById(obj.getId());
+		
+		UpdateData(newObj,obj);
+		return repository.save(newObj);
+	}
+	
+	private void UpdateData(User newObj, User obj) 
+	{
+		newObj.setName(obj.getName());
+    	newObj.setEmail(obj.getEmail());	
+		
+	}
+
 	public User fromDTO(UserDTO objDTO)
 	{
 		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
-	
-	
 
 }
